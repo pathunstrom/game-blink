d6215ac65c2234461414dcf44f2dc0c6e63ea039
Piper Thunstrom <pathunstrom@gmail.com>
2020-06-06 17:24:16 -0400

    Adds blinking effect.

--- src/blink/field.py
+++ src/blink/field.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 import ppb
 
 from blink.firefly import Firefly
@@ -7,4 +9,5 @@ class Field(ppb.BaseScene):
     background_color = (30, 0, 75)
 
     def on_scene_started(self, event, signal):
-        self.add(Firefly())
+        for x, y in product(range(-3, 4, 2), range(-3, 4, 2)):
+            self.add(Firefly(position=ppb.Vector(x, y)))
--- src/blink/firefly.py
+++ src/blink/firefly.py
@@ -1,7 +1,57 @@
+
+from random import randint
+
 import ppb
 from ppb.features import animation
+
+
+def _one():
+    return 1
+
+
+def _half():
+    return 0.5
+
+
+_urge_increase_functions = {
+    "half": _half,
+    "one": _one
+}
+
+
+def _urge_increase(key="one"):
+    return _urge_increase_functions[key]()
 
 
 class Firefly(ppb.Sprite):
     size = 0.5
     image = animation.Animation("blink/resources/firefly/{0..5}.png", 30)
+    urge = 0
+    urge_increase = "half"
+    max_urge = 100
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.urge = randint(0, 35)
+
+    def on_update(self, event, signal):
+        self.urge += _urge_increase(self.urge_increase)
+        if self.urge >= self.max_urge:
+            event.scene.add(Light(position=self.position))
+            self.urge -= self.max_urge
+
+
+class Light(ppb.Sprite):
+    image = ppb.Circle(246, 255, 77)
+    _opacity = 0.3
+    size = 4
+    parent = None
+
+    def on_update(self, event, signal):
+        self._opacity -= 0.025
+        if self._opacity <= 0:
+            event.scene.remove(self)
+
+    @property
+    def opacity(self):
+        return int(255 * self._opacity)
