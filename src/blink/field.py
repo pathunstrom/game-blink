5feaf404e07d9f2b8889ee5cf6acf6eda0cb5b71
Piper Thunstrom <pathunstrom@gmail.com>
2020-06-07 19:20:07 -0400

    Minors changes to initial state.

--- src/blink/field.py
+++ src/blink/field.py
@@ -1,6 +1,8 @@
 from itertools import product
+from random import uniform
 
 import ppb
+from ppb import buttons
 
 from blink.firefly import Firefly, MousePosition
 
@@ -9,6 +11,23 @@ class Field(ppb.BaseScene):
     background_color = (30, 0, 75)
 
     def on_scene_started(self, event, signal):
-        for x, y in product(range(-3, 4, 2), range(-3, 4, 2)):
-            self.add(Firefly(position=ppb.Vector(x, y)))
-            self.add(MousePosition())
+        self.add(MousePosition())
+        for _ in range(5):
+            self.spawn_firefly()
+
+    def on_button_pressed(self, event, signal):
+        if event.button is buttons.Secondary and len(list(self.get(kind=Firefly))) < 25:
+            self.spawn_firefly()
+
+    def spawn_firefly(self):
+        spawn = ppb.Vector(uniform(self.main_camera.left, self.main_camera.right), self.main_camera.top + 1)
+        velocity = ppb.Vector(uniform(-1, 1), uniform(-1, 1))
+        if velocity:
+            velocity = velocity.scale_to(Firefly.max_velocity)
+        self.add(
+            Firefly(
+                position=spawn + ppb.Vector(0, -0.5),
+                spawn=spawn,
+                velocity=velocity
+            )
+        )
--- src/blink/firefly.py
+++ src/blink/firefly.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from functools import partial
 from random import randint
 from random import uniform
 from typing import List, NamedTuple
@@ -9,7 +10,18 @@ from ppb.features import animation
 
 
 LIGHT_RADIUS = 2.5
-URGE_MULTIPLIER = 6
+URGE_MULTIPLIER = 20
+
+
+def clamp(x, _min, _max):
+    return max(_min, min(x, _max))
+
+
+def smootherstep(minimum, maximum, current):
+    # Scale, and clamp x to 0..1 range
+    x = clamp((current - minimum) / (maximum - minimum), 0.0, 1.0)
+    # Evaluate polynomial
+    return x * x * x * (x * (x * 6 - 15) + 10)
 
 
 @dataclass
@@ -32,7 +44,8 @@ def _half():
 
 _urge_increase_functions = {
     "half": _half,
-    "one": _one
+    "one": _one,
+    "small_range": partial(uniform, 0.1, 0.5)
 }
 
 
@@ -51,29 +64,31 @@ class Firefly(ppb.Sprite):
     size = 0.5
     image = animation.Animation("blink/resources/firefly/{0..5}.png", 30)
     urge = 0
-    urge_increase = "half"
-    max_urge = 100
+    urge_increase = "small_range"
+    max_urge = 200
     velocity: ppb.Vector = ppb.Vector(0, 1)
     max_velocity = 0.75
     wander_vector: ppb.Vector
     basis = ppb.Vector(0, 1)
     layer = 3
     _wall_buffer = 2
-    _others_buffer = 4
+    _others_buffer = 3
     _flocking_distance = 10
-    _mouse_buffer = 4
+    _mouse_buffer = 5
+    spawn_point: ppb.Vector
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.intialize_urge()
+        self.initialize_urge()
         self.wander_vector = ppb.Vector(0, 1).rotate(randint(0, 359))
+        self.spawn_point = getattr(self, "spawn_point", self.position)
 
     def on_update(self, event, signal):
         self.urge += _urge_increase(self.urge_increase)
         if self.urge >= self.max_urge:
             event.scene.add(Light(position=self.position))
             signal(Blink(self))
-            self.intialize_urge()
+            self.urge = 0
 
         forces: List[Force] = []
         self.wander(forces)
@@ -87,7 +102,15 @@ class Firefly(ppb.Sprite):
         mouse = next(event.scene.get(kind=MousePosition))
         avoid_mouse = self.flee_if_near(mouse.position, self._mouse_buffer)
         if avoid_mouse:
-            forces.append(Force(avoid_mouse, 3))
+            forces.append(Force(avoid_mouse, 6))
+
+        seek_center = self.seek_if_far(ppb.Vector(0, 0), 8)
+        if seek_center:
+            forces.append(Force(seek_center, 5))
+
+        avoid_spawn = self.flee_if_near(self.spawn_point, 2)
+        if avoid_spawn:
+            forces.append(Force(avoid_spawn, 10))
 
         force_vector = ppb.Vector(0, 0)
         weights = 0
@@ -100,13 +123,17 @@ class Firefly(ppb.Sprite):
         self.facing = self.velocity.normalize()
         self.velocity *= .998
 
+    def on_button_pressed(self, event, signal):
+        if (event.position - self.position).length <= self._mouse_buffer:
+            self.initialize_urge()
+
     def on_blink(self, event, signal):
         distance = (self.position - event.source.position).length
         if event.source is not self and distance <= LIGHT_RADIUS:
             self.urge += sum(_urge_increase(self.urge_increase) for _ in range(URGE_MULTIPLIER))
 
-    def intialize_urge(self):
-        self.urge = randint(0, 55)
+    def initialize_urge(self):
+        self.urge = uniform(0, self.max_urge)
 
     def wander(self, forces):
         self.wander_vector = self.wander_vector.rotate(uniform(-0.5, 0.5))
@@ -143,7 +170,7 @@ class Firefly(ppb.Sprite):
         forces.append(
             Force(
                 self.flee(local_center),
-                self._others_buffer - (local_center - self.position).length * 2
+                3
             )
         )
 
@@ -176,19 +203,25 @@ class Firefly(ppb.Sprite):
             return self.flee(point)
         return ppb.Vector(0, 0)
 
+    def seek_if_far(self, point, distance):
+        if (self.position - point).length >= distance:
+            return self.seek(point)
+        return ppb.Vector(0, 0)
+
 
 class Light(ppb.Sprite):
     image = ppb.Circle(246, 255, 77)
     _opacity = 0.3
-    size = LIGHT_RADIUS * 2
+    _max_opacity = 0.3
+    size = LIGHT_RADIUS * 2.2
     parent = None
     layer = 5
 
     def on_update(self, event, signal):
-        self._opacity -= 0.025
+        self._opacity -= 0.0125
         if self._opacity <= 0:
             event.scene.remove(self)
 
     @property
     def opacity(self):
-        return int(255 * self._opacity)
+        return int(255 * smootherstep(0, self._max_opacity, self._opacity) * self._max_opacity)
